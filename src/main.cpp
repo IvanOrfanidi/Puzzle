84c0545c01f8ae989789c86335c2c39eccdd4e87
Ivan Orfanidi <i.orfanidi@mail.ru>
2020-10-23 15:42:21 +0300

    Добавлено получение версии файла из CMakeLists.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,16 @@
-#include "../include/mainwindow.h"
+#include <project_version.h>
+#include <include/mainwindow.h>
 #include <QApplication>
 #include <cstdint>
 #include <iostream>
 #include <string>
-
-const std::string VERSION = "1.0.1";
 
 std::string parseCommand(int argc, char* argv[])
 {
     for (int i = 0; i < argc; ++i) {
         std::string arg = argv[i];
         if (arg == "-v" || arg == "--version") {
-            return VERSION;
+            return PROJECT_VERSION;
         }
     }
     return "";
